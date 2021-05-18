da2dd5ec6a77ab530aaf0a290687293d8070bde4
vantrong291 <vantrong291@gmail.com>
2021-05-18 18:14:07 +0700

    [Configs] updated configs

--- src/RpcClient.ts
+++ src/RpcClient.ts
@@ -1,50 +1,50 @@
-import { readFileSync } from "fs";
-import { Agent } from "https";
+import {readFileSync} from "fs";
+import {Agent} from "https";
 import axios from "axios";
 
 type Protocol = "https" | "http";
 
 interface ChiaOptions {
-  protocol: Protocol;
-  hostname: string;
-  port: number;
-  caCertPath: string | boolean;
-  certPath: string;
-  keyPath: string;
+    protocol: Protocol;
+    hostname: string;
+    port: number;
+    caCertPath: string | boolean;
+    certPath: string;
+    keyPath: string;
 }
 
 class RpcClient {
-  private readonly protocol: Protocol;
-  private readonly hostname: string;
-  private readonly port: number;
-  private readonly agent: Agent;
+    private readonly protocol: Protocol;
+    private readonly hostname: string;
+    private readonly port: number;
+    private readonly agent: Agent;
 
-  public constructor(options: ChiaOptions) {
-    this.protocol = options.protocol;
-    this.hostname = options.hostname;
-    this.port = options.port;
-    this.agent = new Agent({
-      ...(typeof options.caCertPath !== 'boolean' ? { ca: readFileSync(options.caCertPath) } : {}),
-      cert: readFileSync(options.certPath),
-      key: readFileSync(options.keyPath),
-      rejectUnauthorized: options.hostname !== "0.0.0.0",
-    });
-  }
+    public constructor(options: ChiaOptions) {
+        this.protocol = options.protocol;
+        this.hostname = options.hostname;
+        this.port = options.port;
+        this.agent = new Agent({
+            ...(typeof options.caCertPath !== 'boolean' ? {ca: readFileSync(options.caCertPath)} : {}),
+            cert: readFileSync(options.certPath),
+            key: readFileSync(options.keyPath),
+            rejectUnauthorized: !["0.0.0.0", process.env['CHIA_POOL_HOST']].includes(options.hostname),
+        });
+    }
 
-  private baseUri(): string {
-    return `${this.protocol}://${this.hostname}:${this.port}`;
-  }
+    private baseUri(): string {
+        return `${this.protocol}://${this.hostname}:${this.port}`;
+    }
 
-  protected async request<T>(
-    route: string,
-    body: Record<string, string | number | boolean | string[] | undefined>
-  ): Promise<T> {
-    const { data } = await axios.post<T>(`${this.baseUri()}/${route}`, body, {
-      httpsAgent: this.agent,
-    });
+    protected async request<T>(
+        route: string,
+        body: Record<string, string | number | boolean | string[] | undefined>
+    ): Promise<T> {
+        const {data} = await axios.post<T>(`${this.baseUri()}/${route}`, body, {
+            httpsAgent: this.agent,
+        });
 
-    return data;
-  }
+        return data;
+    }
 }
 
-export { ChiaOptions, RpcClient };
+export {ChiaOptions, RpcClient};
